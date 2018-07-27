951d9a4a2becbc50ac7d50f6d2acf067c2d7e6f5
Ronald Tagolino <tags@Ronaldde-MacBook-Pro.local>
2018-07-27 20:37:28 +0800

    Add test_orders unit testing.

---
+++ test_brand_orders.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class BrandOrdersTestCase(unittest.TestCase):
+    """
+        Tests for brand_orders.py
+    """
+
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()

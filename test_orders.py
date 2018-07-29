1975c90ee18392ce20bf8375a6d655e2efab2ea0
Ronald Tagolino <tags@Ronaldde-MacBook-Pro.local>
2018-07-29 12:40:33 +0800

    Update test_orders. Add initial test cases.

--- test_orders.py
+++ test_orders.py
@@ -6,7 +6,17 @@ class OrdersTestCase(unittest.TestCase):
         Tests for brand_orders.py
     """
 
-    pass
+    def test_is_valid_orders(self):
+        pass
+
+    def test_is_invalid_orders(self):
+        pass
+
+    def test_parse_orders(self):
+        pass
+
+    def compute_orders(self):
+        pass
 
 
 if __name__ == '__main__':
